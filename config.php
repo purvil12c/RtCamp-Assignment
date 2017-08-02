d53cb99fa59045fa915fe23073c98c5cb50b4ddf
purvil12c <purvil12c@gmail.com>
2017-08-02 17:16:41 +0530

    Twitter redirect

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+
+    'consumer_key'      => '1jT61PmX0GsxQ6XIt2mXVdKuD',
+    'consumer_secret'   => 'QPVyPpukRURxL4p441XoF4a8gl4VBj59aLuu7XBq0oAqlCIlzy',
+
+    'url_login'         => 'http://localhost/index.php',
+    'url_callback'      => 'http://localhost/twitter_callback.php',
+];
