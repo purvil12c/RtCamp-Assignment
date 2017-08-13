2e3fe646a0610e8fdbe7fda07ef7ef09e20b5ab8
purvil12c <purvil12c@gmail.com>
2017-08-13 23:21:32 +0530

    changed look of slider

--- getusertimeline.php
+++ getusertimeline.php
@@ -26,7 +26,7 @@
     foreach ($tweets as $tweet) {
         $user = $tweet->user;
         $screenname = $user->screen_name;
-        echo "<li style='margin:50px;'><blockquote>
+        echo "<li style='margin-left:50px;margin-right:50px;'><blockquote>
                              <p>$tweet->text</p>
                              <footer>$screenname</footer>
                            </blockquote></li>";
